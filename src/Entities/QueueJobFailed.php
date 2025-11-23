cc3c6c4b194ca39c4cc37333d0a11ac3ea1fb2b3
Yassine Doghri <doghri.yassine@gmail.com>
2025-11-23 13:27:39 +0100

    docs: add @property tags to QueueJobFailed entity (#70)
    * docs: add @property tags to QueueJobFailed entity
    
    * chore: run cs-fix to fix styles

--- src/Entities/QueueJobFailed.php
+++ src/Entities/QueueJobFailed.php
@@ -14,7 +14,17 @@ declare(strict_types=1);
 namespace CodeIgniter\Queue\Entities;
 
 use CodeIgniter\Entity\Entity;
+use CodeIgniter\I18n\Time;
 
+/**
+ * @property string $connection
+ * @property string $exceptions
+ * @property Time   $failed_at
+ * @property int    $id
+ * @property array  $payload
+ * @property string $priority
+ * @property string $queue
+ */
 class QueueJobFailed extends Entity
 {
     protected $dates = ['failed_at'];
